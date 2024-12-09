d1f7da6c021e88994df14cc338af6ccd181e47d0
Johann-Gael <johann.lopez@uabc.edu.mx>
2024-12-08 22:14:34 -0800

    Update README.md

--- Dashboard 1.py
+++ Dashboard 1.py
@@ -2,8 +2,16 @@ import pandas as pd
 import plotly.express as px
 
 #Conexion a la base de datos
-use="root"
+user="root"
 password="12345678"
 server="localhost"
+db="adidas_scrapper"
+
+#Cadena que se usara para realizar la conexion
+cadena_conexion = f"mysql+mysqlconnector://{user}:{password}@{server}/{db}"
+
+#Establecer
 
 
+sql="SELECT * FROM ()"
+data=pd.read_sql(sql,)
