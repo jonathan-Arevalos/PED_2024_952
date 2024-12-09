a343e8bdd102a8f002363f99ef152a630e8b9aae
Johann-Gael <johann.lopez@uabc.edu.mx>
2024-12-08 23:09:05 -0800

    Update README.md

--- Dashboard 1.py
+++ Dashboard 1.py
@@ -1,17 +1,42 @@
 import pandas as pd
 import plotly.express as px
-
-#Conexion a la base de datos
-user="root"
-password="12345678"
-server="localhost"
-db="adidas_scrapper"
-
-#Cadena que se usara para realizar la conexion
-cadena_conexion = f"mysql+mysqlconnector://{user}:{password}@{server}/{db}"
-
-#Establecer
+from dash import dcc, html, callback, Input, Output
+from sqlalchemy import create_engine
 
 
-sql="SELECT * FROM ()"
-data=pd.read_sql(sql,)
+# Conexión a la base de datos
+user = "root"
+password = "12345678"
+server = "localhost"
+db = "adidas_scrapper"
+
+# Cadena que se usara para realizar la conexión con pymysql
+cadena_conexion = f"mysql+pymysql://{user}:{password}@{server}/{db}"
+
+# Establecer conexión con engine
+engine = create_engine(cadena_conexion)
+conexion = engine.connect()
+
+sql = "SELECT * FROM INFO"
+data = pd.read_sql(sql, conexion)
+
+conexion.close()
+
+def histograma():
+    fig=px.histogram(data,x="Precio",nbins=10,title="HISTOGRAMA DE PRECIOS",template="plotly_dark")
+    fig.update_layout(xaxis_title="Precio",yaxis_title="Cantidad")
+
+    fig_box=px.scratter(
+        data,
+        x="Precio",
+        y="Cantidad",
+        title="BOXPLOT DE PRECIOS",
+        template="plotly_dark"
+    )
+    fig_box.update_layout(xaxis_title="Precio",yaxis_title="Cantidad")
+
+    body=html.Div([
+
+    ])
+
+    return body
